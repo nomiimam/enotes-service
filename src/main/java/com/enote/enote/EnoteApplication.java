a2de21aa084ef0648f58563324ab27afa2cfafd0
NOMAN AHMED <nomiimam@gmail.com>
2025-02-26 01:56:50 +0500

    auditing

--- src/main/java/com/enote/enote/EnoteApplication.java
+++ src/main/java/com/enote/enote/EnoteApplication.java
@@ -1,9 +1,12 @@
 package com.enote.enote;
 
+import jakarta.persistence.EntityListeners;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAware")
 public class EnoteApplication {
 
     public static void main(String[] args) {
